6494667dca19af48dfe8d7212ef85dbb85a4b0b4
Housey <housebreaker1@gmail.com>
2015-11-10 04:54:17 +0200

    add initial variables for 3rd problem

--- Java Fundamentals Retake - 26 October 2015/src/p03_TheHeiganDance/TheHeiganDance.java
+++ Java Fundamentals Retake - 26 October 2015/src/p03_TheHeiganDance/TheHeiganDance.java
@@ -2,6 +2,12 @@ package p03_TheHeiganDance;
 
 public class TheHeiganDance {
 	public static void main(String[] args) {
+		int[][] matrix = new int[15][15];
+
+		int playerHealth = 18500;
+		double heiganHealth = 3000000D;
+
+		int[] playerPosition = {7, 7};
 		// todo
 	}
 }
